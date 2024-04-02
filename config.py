c27cfe277aa6b85d2b89cf0c54bde06bd8313ff2
zhaoyxdt <zhaoyx2014@hotmail.com>
2024-04-03 00:55:16 +0800

    from https://github.com/xfgryujk/blivechat/pull/112

--- config.py
+++ config.py
@@ -78,6 +78,8 @@ class AppConfig:
         self.translation_cache_size = 50000
         self.translator_configs = []
 
+        self.bilibili_cookies_file = ''
+
         self.text_emoticons = []
 
     @property
@@ -132,6 +134,7 @@ class AppConfig:
         self.allow_translate_rooms = _str_to_list(app_section.get('allow_translate_rooms', ''), int, set)
         self.translate_max_queue_size = app_section.getint('translate_max_queue_size', self.translate_max_queue_size)
         self.translation_cache_size = app_section.getint('translation_cache_size', self.translation_cache_size)
+        self.bilibili_cookies_file = app_section.get('bilibili_cookies_file', fallback=self.bilibili_cookies_file)
 
     def _load_translator_configs(self, config: configparser.ConfigParser):
         app_section = config['app']
--- utils/request.py
+++ utils/request.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+import os
+import logging
+import http.cookies
+
 import asyncio
 from typing import *
 
 import aiohttp
+
+import config
+
+logger = logging.getLogger(__name__)
 
 # 不带这堆头部有时候也能成功请求，但是带上后成功的概率更高
 BILIBILI_COMMON_HEADERS = {
@@ -15,11 +23,36 @@ BILIBILI_COMMON_HEADERS = {
 http_session: Optional[aiohttp.ClientSession] = None
 
 
+def load_bilibili_cookies():
+    config.init()
+    cfg = config.get_config()
+    cookies = http.cookies.SimpleCookie()
+    if cfg.bilibili_cookies_file:
+        cookies_fn = os.path.join(config.BASE_PATH, cfg.bilibili_cookies_file)
+        try:
+            with open(cookies_fn, 'rt') as f:
+                cookies.load(f.read())
+            for cookie in cookies.values():
+                cookie['domain'] = cookie['domain'] or '.bilibili.com'
+        except FileNotFoundError:
+            logger.warning("Cookies file not found, check if config is correct")
+        for key in ['SESSDATA', 'bili_jct']:
+            if key not in cookies:
+                logger.warning("Missing necessary cookie entries, please check cookie file content and format")
+                break
+    return cookies
+
 def init():
     global http_session
+    
+    cookie_jar = aiohttp.CookieJar()
+    cookie_jar.update_cookies(load_bilibili_cookies())
+    #http_session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10), cookie_jar=cookie_jar)
+    
     http_session = aiohttp.ClientSession(
         response_class=CustomClientResponse,
         timeout=aiohttp.ClientTimeout(total=10),
+        cookie_jar=cookie_jar
     )
 
 
